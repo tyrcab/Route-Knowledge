1af8e0a7dde982be82df0dcd9a4286b0dfd067e0
tyrcab <tyroncabral@gmail.com>
2025-10-24 17:46:09 +1100

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,19 +1,23 @@
-self.addEventListener('install', event => {
+const CACHE_NAME = "route-knowledge-v2";
+const FILES_TO_CACHE = [
+  "/",
+  "/index.html",
+  "/style.css",
+  "/script.js",
+  "/manifest.json",
+  "/data/manifest.json"
+];
+
+self.addEventListener("install", event => {
   event.waitUntil(
-    caches.open('route-knowledge-v1').then(cache => {
-      return cache.addAll([
-        '/',
-        '/index.html',
-        '/style.css',
-        '/script.js',
-        '/manifest.json'
-      ]);
-    })
+    caches.open(CACHE_NAME).then(cache => cache.addAll(FILES_TO_CACHE))
   );
 });
 
-self.addEventListener('fetch', event => {
+self.addEventListener("fetch", event => {
   event.respondWith(
-    caches.match(event.request).then(response => response || fetch(event.request))
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
   );
 });
